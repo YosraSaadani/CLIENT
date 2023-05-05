a97eae8213beaa55bf434e582f7784423c36d4bf
Zied Meddeb <ziedmeddeb123@outlook.com>
2023-05-05 12:30:38 +0100

    name

---
+++ src/app/Components/patient/selected-doctor/selected-doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-doctor',
+  templateUrl: './selected-doctor.component.html',
+  styleUrls: ['./selected-doctor.component.scss']
+})
+export class SelectedDoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

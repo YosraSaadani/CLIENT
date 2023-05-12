44f20385dd7aca55e7652e134a62d9c9a4fcc5ec
- Amine - <mzoughi.mine@gmail.com>
2023-05-12 19:18:37 +0100

    patient validators

--- src/app/Components/patient/login-patient/login/login.component.ts
+++ src/app/Components/patient/login-patient/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PatientService } from 'src/app/services/patient.service';
 
@@ -17,10 +17,22 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm=this.fb.nonNullable.group({
-      email:[''],
-      password:['']
+      email:['',[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password:['',[Validators.required]]
     });
   }
+
+  
+  get email()
+  {
+    return this.loginForm.get('email');
+  }
+  get password()
+  {
+    return this.loginForm.get('password');
+  }
+
+
   login(){
     this.servicePatient.loginPatient(this.loginForm.value).subscribe(data=>{
       localStorage.setItem('patientToken', data.token);
